a0e104695e6032869fc49e015066aca585576831
sivaji.v <sivaji.v@apcfss.in>
2025-11-02 20:04:04 +0530

    url changes

--- src/Container/Bees/employeeurls.js
+++ src/Container/Bees/employeeurls.js
@@ -1,4 +1,7 @@
-export const  GET_EMPLOYEE="http://localhost:3004/employee/employeelist"
-export const  SAVE_EMPLOYEE="http://localhost:3004/employee/saveEmp"
-export const  UPDATE_EMPLOYEE="http://localhost:3004/employee/updateEmp/"
-export const  DELETE_EMPLOYEE="http://localhost:3004/employee/deleteEmp/"
+
+const BASE_URL="https://employeetaskfrontend-mhur.vercel.app/"
+
+export const  GET_EMPLOYEE=BASE_URL+"employee/employeelist"
+export const  SAVE_EMPLOYEE=BASE_URL+"employee/saveEmp"
+export const  UPDATE_EMPLOYEE=BASE_URL+"employee/updateEmp/"
+export const  DELETE_EMPLOYEE=BASE_URL+"employee/deleteEmp/"
